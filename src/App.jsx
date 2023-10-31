738a0ec94bdbe68332cf36bd835d4df101ff3d16
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2023-10-30 22:46:43 -0600

    feat: 🎸 start project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+ return (
+  <div className="bg-gradient-to-r from-[hsl(192,100%,67%)] to-[hsl(280,87%,65%)] w-1/2 mx-auto relative h-11 top-6">
+    <h1 className="font-josefi-sans text-bright-blue">Todo App</h1>
+  </div>
+ )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'josefi-sans': ['Josefin Sans', 'sans-serif']
+      },
+      colors: {
+        /* Primary */
+        'bright-blue': 'hsl(220, 98%, 61%)',        
+
+        /* Light Theme */
+        'very-light-gray': 'hsl(0, 0%, 98%)',
+        'very-light-grayish-blue': 'hsl(236, 33%, 92%)',
+        'light-grayish-blue': 'hsl(233, 11%, 84%)',
+        'dark-grayish-blue': 'hsl(236, 9%, 61%)',
+        'very-dark-grayish-blue': 'hsl(235, 19%, 35%)'
+      },
+      backgroundImage: {
+        'check-background': 'linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
